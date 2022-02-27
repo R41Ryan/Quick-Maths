89704224a28f6591b858602210efd32ab659fd70
R41Ryan <ryanhuynh64@gmail.com>
2022-02-27 08:06:52 -0700

    Turned the boundary prompts and addition test into functions
    I also added the option of a minimum boundary.

--- Quick Maths.cpp
+++ Quick Maths.cpp
@@ -1,39 +1,48 @@
-// Quick Maths.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
+// Clears the input buffer.
 void clear() {
     while (getchar() != '\n');
 }
 
-int main()
-{
+/*
+* Prompts the user to input the min and max absolute boundaries for the operands.
+*/
+void promptBoundaries(int& min, int& max) {
     std::cout << "Please enter the maximum value for operand\n";
-    int max;
     std::cin >> max;
-    srand(time(NULL));
-    int operand1 = rand() % max;
-    int operand2 = rand() % max;
+    clear();
+    std::cout << "Please enter the minimum value for operand\n";
+    std::cin >> min;
+    clear();
+}
+
+/*
+* Provides a random integer addition question for the user to inputand returns either true or false, depending on whether they got it right.
+* min and max indicate the lowest and highest absolute values for the addends.
+*/
+bool testIntAddition(int min, int max) {
+    srand(time(0));
+    int operand1 = rand() % max + min;
+    int operand2 = rand() % max + min;
     int answer = operand1 + operand2;
     int userInput;
     std::cout << operand1 << " + " << operand2 << " = ?" << std::endl;
     std::cin >> userInput;
+    clear();
     if (userInput == answer) {
         std::cout << "Correct!" << std::endl;
+        return true;
     }
     else {
         std::cout << "Incorrect! Answer was " << answer << std::endl;
+        return false;
     }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    int max, min;
+    promptBoundaries(min, max);
+    testIntAddition(min, max);
+}
